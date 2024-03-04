292e939eb8dfdaacb2cda1569b0dd63269d5ca0c
Abhishek Chauhan <abhishekkumar.chauhan@philips.com>
2024-03-04 15:57:41 +0530

    small changes for only color test

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask
 from flask import render_template
-import socket
 import random
 import os
 
@@ -22,17 +21,17 @@ color = os.environ.get('APP_COLOR') or random.choice(["red","green","blue","blue
 def main():
     #return 'Hello'
     print(color)
-    return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
+    return render_template('hello.html', color=color_codes[color])
 
 @app.route('/color/<new_color>')
 def new_color(new_color):
-    return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
+    return render_template('hello.html', color=color_codes[new_color])
 
 @app.route('/read_file')
 def read_file():
     f = open("/data/testfile.txt")
     contents = f.read()
-    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
+    return render_template('hello.html', contents=contents, color=color_codes[color])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8080")
